3647aec9d7ed5236dbb821e23a571e8da21be969
Thai Nguyen <thainq00@gmail.com>
2023-01-16 16:25:28 +0700

    add camera

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,26 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'dart:async';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+import 'package:camera/camera.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final TextEditingController _controller = TextEditingController();
+  HomePageState createState() => HomePageState();
+}
 
+class HomePageState extends State<HomePage> {
+  int _selectedTabIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Obtain a list of the available cameras on the device.
+    final cameras = await availableCameras();
+
+    // Get a specific camera from the list of available cameras.
+    final firstCamera = cameras.first;
     return Material(
         child: Align(
-            alignment: Alignment.topLeft,
-            child: SafeArea(
-                child: Column(children: [
-              TextField(
-                controller: _controller,
-                decoration: const InputDecoration(hintText: 'Type sth', labelText: 'Text input'),
-              ),
-              ElevatedButton(
-                onPressed: () => context.goNamed('setting'),
-                child: const Text('Submit'),
-              ),
-            ]))));
+      alignment: Alignment.topLeft,
+      child: SafeArea(
+        child: Scaffold(
+            body: Stack(
+              children: [
+                Offstage(
+                  offstage: _selectedTabIndex != 0,
+                  child: TickerMode(
+                    enabled: _selectedTabIndex == 0,
+                    child: const TextField(),
+                  ),
+                ),
+                Offstage(
+                    offstage: _selectedTabIndex != 1,
+                    child: TickerMode(
+                      enabled: _selectedTabIndex == 1,
+                      child: TakePictureScreen(camera: camera),
+                    )),
+                Offstage(
+                    offstage: _selectedTabIndex != 2,
+                    child: TickerMode(
+                      enabled: _selectedTabIndex == 2,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            context.goNamed('setting');
+                          },
+                          child: Text('Go setting')),
+                    )),
+              ],
+            ),
+            extendBody: true,
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.call),
+                  label: 'Calls',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.camera),
+                  label: 'Camera',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.chat),
+                  label: 'Chats',
+                ),
+              ],
+              currentIndex: _selectedTabIndex,
+              onTap: _onItemTapped,
+            )),
+      ),
+    ));
   }
 }
