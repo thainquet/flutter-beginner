706cdf475319e185ad1e07ae075a1963b9468fea
Thai Nguyen <thainq00@gmail.com>
2023-01-16 21:32:27 +0700

    update setting and fix warning

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ Future<void> main() async {
 }
 
 class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
