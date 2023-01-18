f9ec05f05e652851a61d0302ee6e5a340490bb13
Thai Nguyen <thainq00@gmail.com>
2023-01-18 16:57:35 +0700

    add config for firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ Future<void> main() async {
   );
 
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await messaging.getInitialMessage();
 
   NotificationSettings settings = await messaging.requestPermission(
     alert: true,
@@ -24,12 +25,33 @@ Future<void> main() async {
   );
 
   print('User granted permission: ${settings.authorizationStatus}');
+  FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true, badge: true, sound: true);
 
   runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => MainAppState();
+}
+
+class MainAppState extends State<MainApp> {
+  String? mtoken = '';
+  @override
+  void initState() {
+    super.initState();
+    getToken();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,5 +59,14 @@ class MainApp extends StatelessWidget {
       title: 'Main app',
       routerConfig: router(),
     );
+  }
+
+  void getToken() async {
+    await FirebaseMessaging.instance.getToken().then((value) => {
+          setState((() {
+            mtoken = value;
+            print('My fcm token: $mtoken');
+          }))
+        });
   }
 }
